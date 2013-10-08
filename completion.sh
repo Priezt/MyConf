2d534154d262b50c2269a70ece926b156e164f6f
Priezt <vegetabird@gmail.com>
2013-10-08 00:58:41 -0700

    rake completion

--- completion.sh
+++ completion.sh
@@ -1,2 +1,11 @@
-complete -W "$(rake -T | awk '{print $2}')" rake
+#complete -W "$(rake -T | awk '{print $2}')" rake
 
+export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
+
+function _rakecomplete() {
+	local tasks=`rake -T | awk '{print $2}'`
+	COMPREPLY=($(compgen -W "${tasks}" -- ${COMP_WORDS[COMP_CWORD]}))
+	return 0
+}
+
+complete -o default -o nospace -F _rakecomplete rake
