f4a3246db859c1d8c174cd108ee93ec7693e9716
Priezt <vegetabird@gmail.com>
2014-10-04 03:14:32 -0400

    also link .gitignore while deploying

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,13 @@ else
 	ln -s ~/MyConf/.gitconfig ~/.gitconfig
 fi
 
+if [ -e ~/.gitignore ]
+then
+	echo .gitignore already exists
+else
+	ln -s ~/MyConf/.gitignore ~/.gitignore
+fi
+
 if [ -e ~/.tmux.conf ]
 then
 	echo .tmux.conf already exists
