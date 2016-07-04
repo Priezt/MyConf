2248a5beb9bac4c4158f69eaa116b7038e1400c2
Priezt <vegetabird@gmail.com>
2016-07-04 20:38:18 +0800

    add shims for python/perl

--- langs-inc.sh
+++ langs-inc.sh
@@ -1,8 +1,10 @@
 export PATH="$HOME/.plenv/bin:$PATH"
+export PATH="$HOME/.plenv/shims:$PATH"
 eval "$(plenv init -)"
 
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
+export PATH="$PYENV_ROOT/shims:$PATH"
 eval "$(pyenv init -)"
 
 export PATH="$HOME/.rbenv/bin:$PATH"
